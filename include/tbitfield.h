1cf985e64abc28aee392fbc57fd08baf02e6203f
LVladislav <latanov2014@mail.ru>
2015-10-30 23:53:55 +0300

    Revert "BitField"

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -22,15 +22,15 @@ private:
   int  MemLen; // к-во эл-тов Мем для представления бит.поля
 
   // методы реализации
-  int   GetMemIndex(const int n) const; // индекс в pМем для бита n                         (#О2)
-  TELEM GetMemMask (const int n) const; // битовая маска для бита n                         (#О3)
+  int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
+  TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
 public:
-  TBitField(const int len);          //инициализирующий конструктор (выделение памяти)      (#О1)
-  TBitField(const TBitField &bf);    //копирующий конструктор                               (#П1)
-  ~TBitField();                      // диструктор                                          (#С)
+  TBitField(int len);                //                                   (#О1)
+  TBitField(const TBitField &bf);    //                                   (#П1)
+  ~TBitField();                      //                                    (#С)
 
   // доступ к битам
-  int GetLength(void) const;      // получить длину (к-во битов)           (#О) const стоит, потому что функцию нельзя изменять. А в скобках, потому что аргумент нельзя изменять
+  int GetLength(void) const;      // получить длину (к-во битов)           (#О)
   void SetBit(const int n);       // установить бит                       (#О4)
   void ClrBit(const int n);       // очистить бит                         (#П2)
   int  GetBit(const int n) const; // получить значение бита               (#Л1)
